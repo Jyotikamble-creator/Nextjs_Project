07c6fd4351ebb16ca874d282a8e5b0531d76d0d8
Jyoti kamble <jyotikamble5235@gmail.com>
2025-06-15 19:51:21 +0530

    feat(lib):credentials

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,82 @@
+import User from "@/models/User";
+import bcrypt from "bcryptjs"
 import { NextAuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectionToDatabase } from "./db";
+
+// credentials{email,password}
 
 export const authOptions:NextAuthOptions={
+   providers:[
+      CredentialsProvider({
+         name:"Credentials",
+         credentials:{
 
-     providers: [
-         GithubProvider({      
-            clientId: process.env.GITHUB_ID!,      
-            clientSecret: process.env.GITHUB_SECRET!,   
-         }),    
-        ]
-     
+           email: {label:"Email" ,type:"text"},
+           password:{label:"Password",type:"text"}
+         },
+
+         async authorize(credentials){
+            if(!credentials?.email||!credentials?.password){
+               throw new Error("missing email or password")
+            }
+
+            try {
+               await connectionToDatabase()
+               const user=await User.findOne({email:credentials.email})
+
+
+               if(!user){
+                  throw new Error("user not found")
+               }
+
+               const isVlaid=await bcrypt.compare(
+                  credentials.password,
+                  user.password
+               )
+
+                          if(!isVlaid){
+                  throw new Error("invalid password")
+               }
+
+               // next auth return
+               return{
+                  id:user._id.toString(),
+                  email:user.email
+                  
+               }
+
+            } catch (error) {
+               console.log("auth error",error)
+               throw error
+            }
+         }
+      })
+   ]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import GithubProvider from "next-auth/providers/github"
+
+// export const authOptions:NextAuthOptions={
+
+//      providers: [
+//          GithubProvider({      
+//             clientId: process.env.GITHUB_ID!,      
+//             clientSecret: process.env.GITHUB_SECRET!,   
+//          }),    
+//         ]
+     
+// }
