0e2475f6d9fe74781e63483493dc4a4377908e47
Jyoti kamble <jyotikamble5235@gmail.com>
2025-06-17 13:55:48 +0530

    feat(components):upload

---
+++ app/components/FileUpload.tsx
@@ -0,0 +1,114 @@
+"use client"
+import {
+    ImageKitAbortError,
+    ImageKitInvalidRequestError,
+    ImageKitServerError,
+    ImageKitUploadNetworkError,
+    upload,
+} from "@imagekit/next";
+import { useRef, useState } from "react";
+
+
+
+interface FileUploadProps {
+    onSuccess: (res: any) => void
+    onProgress?: (progress: number) => void
+    fileType?: "image" | "video"
+}
+
+const FileUpload = ({ onSuccess, onProgress, fileType }: FileUploadProps) => {
+
+    const [uploading, setUploading] = useState(false)
+
+    const [error, setError] = useState<string | null>(null)
+
+
+    // validation
+
+    const validateFile = (file: File) => {
+        if (fileType === "video") {
+            if (!fileType.type.startWith("video/")) {
+                setError("upload valid file")
+
+            }
+
+            if (file.size > 100 * 1024 * 1024) {
+                setError("file size must be less than 100MB")
+            }
+        }
+
+        return true;
+
+    }
+
+
+
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+
+        const file = e.target.files?.[0]
+
+        if (!file || !validateFile(file)) return
+
+        setUploading(true)
+        setError(null)
+
+        try {
+            const authRes = await fetch("/api/auth/imageKit-auth")
+            const auth = await authRes.json()
+
+
+         const res=   await upload({
+                file,
+                fileName: file.name,
+                publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+                signature: auth.signature,
+                expire: auth.expire,
+                token: auth.token,
+
+
+
+                onProgress: (event) => {
+                    if (event.lengthComputable && onProgress) {
+                        const percent = (event.loaded / event.total) *
+                            100
+
+                        onProgress(Math.round(percent))
+                    }
+                },
+
+
+
+            })
+
+            onSuccess(res)
+
+        } catch (error) {
+console.error("uplaod failed",error)
+        }finally{
+            setUploading(false)
+        }
+    }
+
+
+
+
+
+
+
+    return (
+        <>
+
+            <input
+                type="file"
+                accept={fileType === "video" ? "video/*" : "image/*"}
+                onChange={handleFileChange}
+
+            />
+
+            {uploading && (<span>Loading..</span>)}
+
+        </>
+    );
+};
+
+export default FileUpload;
