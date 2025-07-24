4a4332c4e495c06f8d883fc3ee531e151697655e
Jyoti kamble <jyotikamble5235@gmail.com>
2025-07-24 22:23:34 +0530

    feat(context):auth

---
+++ app/context/AuthContext.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+import { createContext, useContext } from "react";
+
+interface AuthContextType {
+  user: any;
+  isLoggedIn: boolean;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  isLoggedIn: false,
+  logout: () => {},
+});
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { data: session } = useSession();
+
+  const value = {
+    user: session?.user,
+    isLoggedIn: !!session,
+    logout: () => signOut(),
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
