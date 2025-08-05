628051b5c176bd21794eb35124e62071c9b7dc69
Jyoti kamble <jyotikamble5235@gmail.com>
2025-08-05 22:13:45 +0530

    feat:middleware

--- middleware .ts
+++ middleware .ts
@@ -1,38 +1,30 @@
 import { withAuth } from "next-auth/middleware"
 import { NextResponse } from "next/server"
 
-
 export default withAuth(
+  function middleware() {
+    return NextResponse.next()
+  },
+  {
+    callbacks: {
+      authorized({ req, token }) {
+        const { pathname } = req.nextUrl
 
-    function middleware() {
-        return NextResponse.next()
-    },
-    {
-        callbacks: {
-            authorized({ req, token }) {
-                const { pathname } = req.nextUrl
-
-                if (
-                    pathname.startsWith("api/auth") ||
-                    pathname === "/login" ||
-                    pathname === "/register"
-                )
-                    return true
-
-                if (pathname === "/" || pathname.startsWith("api/videos")) {
-                    return true
-                }
-
-                // this convert to boolloen form
-                return !!token 
-            }
+        if (pathname.startsWith("/api/auth") || pathname === "/login" || pathname === "/register") {
+          return true
         }
-    }
+
+        if (pathname === "/" || pathname.startsWith("/api/videos")) {
+          return true
+        }
+
+        return !!token
+      },
+    },
+  },
 )
 
-
-export const config={
-    matcher:[
-        "/((?!_next/statisc|_next/image|favicon.icon|public/).*)"
-    ]
+export const config = {
+  // FIX: Fixed typo in matcher
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|public/).*)"],
 }
