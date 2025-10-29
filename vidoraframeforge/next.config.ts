21266d907727d8f1d4f50f1aff1f7b4544638807
Jyoti kamble <jyotikamble5235@gmail.com>
2025-10-29 23:30:40 +0530

    feat(new)whole

--- vidoraframeforge/next.config.ts
+++ vidoraframeforge/next.config.ts
@@ -1,22 +1,37 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
-
+// export default nextConfig;
 
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ["ik.imagekit.io"], // Add your ImageKit domain
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        port: "",
+        pathname: "/**",
       },
     ],
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["localhost:3000"],
+    },
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 }
 
